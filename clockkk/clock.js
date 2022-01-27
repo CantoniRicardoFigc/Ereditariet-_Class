41a8ffe442cc57fa89baad756b5b9684e34048b8
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2022-01-27 13:31:12 +0000

    versione orario complete

--- clockkk/clock.js
+++ clockkk/clock.js
@@ -1,41 +1,41 @@
-export class Clock {
-}
 
-function Clock({ template }) {
-  
-    let timer;
-  
-    function render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    this.stop = function() {
-      clearInterval(timer);
-    };
-  
-    this.start = function() {
-      render();
-      timer = setInterval(render, 1000);
-    };
-  
+export default class Clock {
+  template;
+  timer;
+  constructor(template) {
+    this.template=template;
+    document.getElementById("btn").addEventListener('click', this.start)
   }
+
+  render= ()=> {
+    let date = new Date();
   
-  let clock = new Clock({template: 'h:m:s'});
-  clock.start();
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+      console.log(output)
+      document.getElementById("orario").innerHTML = output;
+  }
+
+  start=()=> {
+    this.render();
+    this.timer = setInterval(this.render, 1000);
+  }
+
+  stop=()=> {
+    clearInterval(this.timer);
+  }
+
+}
 
